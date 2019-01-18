a7eebba813e293851f4048eaffbd59f4366b5b6f
노준호[Gaepury] <njh910505@gmail.com>
2019-01-19 02:49:09 +0900

    [#1] page template 구조 세팅
    - container, component, pages, route 구현
    - 이미지 추가
    - redux ducks 패턴

---
+++ frontend/src/pages/PersonalQuestionsPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PageContainer from '../containers/common/PageContainer';
+import PageTemplate from '../components/common/PageTemplate';
+
+class PersonalQuestionsPage extends Component {
+  render() {
+    return (
+      <PageContainer layout={PageTemplate} >
+      </PageContainer>
+    );
+  }
+};
+
+export default PersonalQuestionsPage;

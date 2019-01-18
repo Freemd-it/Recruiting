11dca12f74023f2605d500c9fb9e06b306829c93
노준호[Gaepury] <njh910505@gmail.com>
2019-01-19 02:49:09 +0900

    [#1] page template 구조 세팅
    - container, component, pages, route 구현
    - 이미지 추가
    - redux ducks 패턴

---
+++ frontend/src/components/common/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import classNames from 'classnames/bind';
+import styles from './Header.scss';
+
+const cx = classNames.bind(styles);
+
+const Header = () => {
+  return (
+    <header className={cx('header')}>
+      bb
+    </header>
+  );
+};
+
+export default Header;

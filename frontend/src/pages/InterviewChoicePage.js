bdb4ad307a6c88bec71e941033a75c01d8e734b8
노준호[Gaepury] <njh910505@gmail.com>
2019-01-19 16:53:08 +0900

    [#1] page config 분리
    - hoc 적용
    - header information text 적용

--- frontend/src/pages/InterviewChoicePage.js
+++ frontend/src/pages/InterviewChoicePage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import PageContainer from '../containers/common/PageContainer';
-import PageTemplate from '../components/common/PageTemplate';
+import { PageCommonTemplate } from '../components/common';
+import { withPageConfig } from '../hoc';
 
 class InterviewChoicePage extends Component {
   render() {
     return (
-      <PageContainer layout={PageTemplate} >
+      <PageContainer pageLayout={ PageCommonTemplate } {...this.props}>
       </PageContainer>
     );
   }
 };
 
-export default InterviewChoicePage;
+export default withPageConfig('interviewChoice')(InterviewChoicePage);

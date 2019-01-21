c79ae7a4095db14acf3f4fa0bffa8c1700f6487d
JunhoeKim <jhkim@hcil.snu.ac.kr>
2019-01-21 15:40:26 +0900

    Add the InterviewNotice component and some common components (#22)
    * Add InterviewNotice component of InterviewChoice section
    
    * Restore inadvertently removed code related to fonts

--- frontend/src/containers/interviewChoice/InterviewChoiceContainer.js
+++ frontend/src/containers/interviewChoice/InterviewChoiceContainer.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom'
 import {  } from '../../components/interviewChoice';
+import SectionTitle from '../../components/common/SectionTitle';
+import InterviewNotice from '../../components/interviewChoice/InterviewNotice/InterviewNotice';
 
 class InterviewChoiceContainer extends Component {
   render() {
@@ -10,9 +12,9 @@ class InterviewChoiceContainer extends Component {
 
     return (
       <>
-
+        <SectionTitle title='인터뷰 시간 선택' />
+        <InterviewNotice />
       </>
-
     );
   }
 }

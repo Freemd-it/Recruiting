256551f496eece9e204c901e33fd574aa99dea1b
노준호[Gaepury] <njh910505@gmail.com>
2019-01-28 00:40:51 +0900

    [#2] 경력, 특기사항 폼 그룹 추가 구현 완료

--- frontend/src/components/personalQuestions/Career/Career.js
+++ frontend/src/components/personalQuestions/Career/Career.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
-import SubsectionHeader from '../../common/SubsectionHeader';
 import CareerFormGroup from '../CareerFormGroup';
+
+import plusImage from '../../../images/plus.png'
 
 import classNames from 'classnames/bind';
 import stylesCommon from '../common/PersonalQuestions.scss';
@@ -11,12 +12,17 @@ const cx = classNames.bind({...stylesCommon, ...styles});
 
 class Career extends Component {
   render() {
-    const { personalFields } = this.props;
+    const { personalFields, onPlusFormGroup } = this.props;
     const { detail } = personalFields.career;
 
     return (
       <div className={cx('personal-form')}>
-        <SubsectionHeader title='경력' />
+        <div className={cx('subsection-title')}>
+          <span>경력</span>
+          <img src={plusImage} onClick={() => onPlusFormGroup('career')}/>
+        </div>
+
+        <hr/>
 
         {detail.map((formGroupRow, formGroupIndex) => (
           <div key={`FormGroup__${formGroupIndex}`}>

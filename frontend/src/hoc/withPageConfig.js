9858f184b7ee8b5970e6a8c141b5f0a45255c49e
노준호[Gaepury] <njh910505@gmail.com>
2019-02-01 01:27:47 +0900

    [#3] 카드 템플릿 구현
    - notosans bold 이슈 수정
    - minus image 추가

--- frontend/src/hoc/withPageConfig.js
+++ frontend/src/hoc/withPageConfig.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+
+import pageStaticData from '../common/pageStaticData';
+import message from '../common/message';
+
 import pageConfig from '../config/pageConfig';
 
 const withPageConfig = (pageConfigName) => (WrappedComponent) => {
   return class extends Component {
     render() {
       return (
-        <WrappedComponent config={pageConfig[pageConfigName]} {...this.props} />
+        <WrappedComponent
+          config={pageConfig[pageConfigName]}
+          messageConfig={message[pageConfigName]}
+          staticData={pageStaticData[pageConfigName]}
+          {...this.props}
+        />
       )
     }
   }

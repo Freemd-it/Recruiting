ec938bbe59fab70de0c0be04e2ca5762263c9183
JunhoeKim <jooon.hoe94@gmail.com>
2019-02-03 21:46:49 +0900

    Add technique select form

--- frontend/src/modules/apply.js
+++ frontend/src/modules/apply.js
@@ -3,9 +3,11 @@ import { fromJS } from 'immutable';
 
 const TEXT_ANSWER_CHANGED = 'apply/TEXT_ANSWER_CHANGED';
 const FILE_ANSWER_CHANGED = 'apply/FILE_ANSWER_CHANGED';
+const SELECT_ANSWER_CHANGED = 'apply/SELECT_ANSWER_CHANGED';
 
 export const textAnswerChanged = createAction(TEXT_ANSWER_CHANGED);
 export const fileAnswerChanged = createAction(FILE_ANSWER_CHANGED);
+export const selectAnswerChanged = createAction(SELECT_ANSWER_CHANGED);
 
 const initialState = fromJS({
   common: {
@@ -28,5 +30,9 @@ export default handleActions({
   [FILE_ANSWER_CHANGED]: (state, action) => {
     const { type, index, name, answerType, file } = action.payload;
     return state.setIn([type, name, index, answerType, 'name'], file.name);
+  },
+  [SELECT_ANSWER_CHANGED]: (state, action) => {
+    const { type, index, name, answerType, techName, abilityIndex } = action.payload;
+    return state.setIn([type, name, index, answerType, techName], abilityIndex);
   }
 }, initialState);

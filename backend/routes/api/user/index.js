5a2cbebf2c33702b5d44f59ecfeff6c3c8c34560
김민수 <whitewolf23@naver.com>
2019-02-06 03:04:39 +0900

    [개발] 2019 02 06
    cors 소스 수정

--- backend/routes/api/user/index.js
+++ backend/routes/api/user/index.js
@@ -6,8 +6,10 @@ router.get('/list', usercontroller.list);
 router.get('/:id', usercontroller.read);
 router.put('/:id', usercontroller.update);
 
+// 리덕스 storedData테스트 
 router.get('/:id/clientStoreData', usercontroller.readStoreData);
 router.put('/:id/clientStoreData', usercontroller.updateStoreData);
 
+// router.post('/:id/upload', user)
 
 module.exports=router;

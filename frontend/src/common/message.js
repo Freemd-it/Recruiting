b96b3b1793cc92f9714b41163fb4fdc7d65187b7
노준호[Gaepury] <njh910505@gmail.com>
2019-02-06 11:31:03 +0900

    [#6] 타입 naming convention 일치 및 임시 저장 버튼 추가

--- frontend/src/common/message.js
+++ frontend/src/common/message.js
@@ -3,6 +3,7 @@ export default {
   SERVER_ERROR: '서버 문제로 로그인이 실패하였습니다.',
   ALREADY_SUBMITTED: '이미 지원서를 제출하였습니다.',
   LOAD_SAVED: '기존에 작성하신 내역이 있습니다. 저장된 내역을 불러오겠습니까?',
+  TEMPORARY_SAVE: '임시 저장이 완료되었습니다',
   INTERVIEW_CHOICE: '각 본부에 맞게 인터뷰 날짜를 최소 2개 이상 선정하셔야 합니다.',
   SUPPORT_CONFIRM: '제출하신 후에는 수정이 불가능합니다. 제출 하시겠습니까?',
   EXIST_EMAIL: '사용중인 이메일입니다. 다른 이메일을 입력해주세요',

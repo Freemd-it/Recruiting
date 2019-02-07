0f34f295d040d49eb0d64bd21ab4ce90c4f037b2
김민수 <whitewolf23@naver.com>
2019-02-07 22:24:52 +0900

    [개발] 2019 02 07
    작업내용
    	1. 질문 GET : api  만듬
    		쿼리스트링으로 ex) ?key=10101_10101 형태로 유저가 선택한 본부의 일종의 키값을 보내주면 질문 return값을 보내줌
    	2. UserModel 에 IT기획본부 언어 능력 관련 스키마 추가 (ex) javascitp : 1, 기본상태 : 3)
    	3. buket name과 awsconfig추가

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const auth = require('./auth');
 const user = require('./user');
+const questions = require('./questions');
 
 const authMiddleware = require('../../middlewares/Auth');
 
@@ -11,5 +12,9 @@ router.use('/auth', auth)
 // 해당 router 에 middleware를 적용
 router.use('/recruits', authMiddleware);
 router.use('/recruits', user);
+
+router.use('/questions', authMiddleware);
+router.use('/questions', questions);
+
 
 module.exports = router;
---
+++ backend/routes/api/questions/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router();
+const questionscontroller = require('../../../controllers/QuestionsController');
+
+router.get('/list', questionscontroller.getQuestionslist);
+
+module.exports = router;

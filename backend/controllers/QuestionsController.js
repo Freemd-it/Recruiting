383de424af813e8e70fb133533b2e6cdd9ded177
김민수 <whitewolf23@naver.com>
2019-02-09 13:28:46 +0900

    [개발 ] 2019 02 09
    작업내용
    1. 파일 업로드 s3연동 완료
    
    이후 작업
    클라이언트와 연동작업

--- backend/controllers/QuestionsController.js
+++ backend/controllers/QuestionsController.js
@@ -2,7 +2,6 @@ const Questions = require('../models/QuestionsModel');
 
 exports.getQuestionslist = async (req, res) => {
   const { key } = req.query;
-  console.log('들어온 값:', key)
   const _key = key === undefined ? [] :  key.split('_').map((item) => {
     return parseInt(item, 10);
   })
--- backend/routes/api/user/index.js
+++ backend/routes/api/user/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const router = express.Router();
 const usercontroller = require('../../../controllers/UserController');
+const filecontroller = require('../../../controllers/FileController');
+const multer = require('multer');
+
+const memorystorage = multer.memoryStorage()
+const uploadData = multer({
+  storage: memorystorage
+})
+
 
 router.get('/list', usercontroller.list);
 router.get('/:id', usercontroller.read);
@@ -10,6 +18,11 @@ router.put('/:id', usercontroller.update);
 router.get('/:id/clientStoreData', usercontroller.readStoreData);
 router.put('/:id/clientStoreData', usercontroller.updateStoreData);
 
-// router.post('/:id/upload', user)
+// 파일 업로드 
+router.get('/:id/file', filecontroller.getFileData)
+// router.get('/:id/upload', filecontroller.getUploadfile)
+// router.put('/:id/uploadlocal', filecontroller.puttUploadfilelocal)
+router.put('/:id/upload', uploadData.array('file'), filecontroller.putUploadData)
+
 
 module.exports=router;

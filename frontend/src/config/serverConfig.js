1c92a3c55d8479ffea45fc07cd11440f286243b9
JunhoeKim <jooon.hoe94@gmail.com>
2019-02-10 01:42:42 +0900

    Change question text format

--- frontend/src/config/serverConfig.js
+++ frontend/src/config/serverConfig.js
@@ -1,5 +1,5 @@
 export default {
   // url: 'http://recruiting.freemedicals.org',
-  // url: 'http://13.209.65.66:3002',
-  url: 'http://localhost:3002'
+  url: 'http://13.209.65.66:3002',
+  // url: 'http://localhost:3002'
 }

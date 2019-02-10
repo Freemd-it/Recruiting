7275d42aad0af4beaf0b0b790f071310ab3ec51a
김민수 <whitewolf23@naver.com>
2019-02-10 16:03:19 +0900

    [개발] 2019 02 10 작업내용 	files[] 을 스키마에 만들어서 file data 저장
    추후작업내용
    포트폴리오 스키마에 req.files를 넣게끔 해야함.

--- backend/lib/s3Connect.js
+++ backend/lib/s3Connect.js
@@ -1,19 +1,28 @@
 const aws = require('aws-sdk')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
+const moment = require('moment');
+
 
 aws.config.loadFromPath('./config/aws_config.json')
+const { envConfig } = require('../config/constants');
+const node_env = process.env.NODE_ENV
+const { PREFIX, BUCKET } = envConfig(node_env)
 
 const S3 = new aws.S3()
+const year = moment().format('YYYY')
+const month = moment().format('MM')
+const day = moment().format('DD')
 
 module.exports = multer({
   storage: multerS3({
     s3 : S3,
-    bucket: 'freemed.recruiting',
+    // bucket : 'freemed.develop',
+    bucket: `${BUCKET}/${PREFIX}/${year}/${month}/${day}`,
     acl: 'public-read',
     key(req, file, cb) {
-      console.log('req', req)
-      console.log('fie', file)
+      // console.log('req', req)
+      // console.log('fie', file)
       cb(null, Date.now() + '.' + file.originalname.split('.').pop())
     },
   }),

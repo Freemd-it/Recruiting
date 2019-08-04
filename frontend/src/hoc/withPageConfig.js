a1253a25eb69a22f960a309d86f3e191bfc6e34d
JunhoeKim <jooon.hoe94@gmail.com>
2019-08-05 00:32:29 +0900

    Remove unused imports

--- frontend/src/hoc/withPageConfig.js
+++ frontend/src/hoc/withPageConfig.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import pageStaticData from '../common/pageStaticData';
 

df0a7e8889e6445952db8f9069c81efc9dba04c1
EC2 Default User <ec2-user@ip-172-31-25-32.ap-northeast-2.compute.internal>
2019-08-09 16:46:46 +0000

    Backend hotfix

--- backend/app.js
+++ backend/app.js
@@ -44,6 +44,7 @@ app.use('/api', api);
 app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  console.log(err);
   res.status(err.status || 500);
   res.json({message : err.message})
 });
